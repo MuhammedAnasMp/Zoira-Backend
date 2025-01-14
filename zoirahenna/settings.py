56f2803a6ef0bab2f8797fe06c038bba41999ed6
anas <muhammedanasmp2001@gmail.com>
2025-01-14 17:01:34 +0530

    stattic files added

--- zoirahenna/settings.py
+++ zoirahenna/settings.py
@@ -120,11 +120,11 @@ USE_I18N = True
 
 USE_TZ = True
 
-
+import os
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
-
-STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
